9c1b0ce4c1a9412ca6471a63c89c0e4a6aff4116
DainikGupta <54498836+DainikGupta@users.noreply.github.com>
2019-11-20 23:07:42 +0530

    Add files via upload

---
+++ PRINT_MSG.c
@@ -0,0 +1,24 @@
+
+#include "stm32f4xx.h"
+#include <string.h>
+#include <stdio.h>
+
+void printMsg(int a)
+{
+	 float a1=* ((float * ) &a);
+	 
+	 char Msg1[100];
+	 
+	 char *ptr;
+	sprintf(Msg1, "The Sigmoid Value is: %f, \n", a1);
+	 
+	 ptr = Msg1 ;
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   
+	 }
+	 ITM_SendChar('\t')	 ;
+   }
+
+	 
